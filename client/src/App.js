07ee77c18204e226b0567d84c496b01085b9fd66
Dariusz Okoński <darek200180@gmail.com>
2023-03-07 18:08:14 +0100

    add basic routes on frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,28 @@
 import { Fragment } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 import Landing from './components/layout/Landing';
 import Navbar from './components/layout/Navbar';
 
 function App() {
   return (
-    <Fragment>
-      <Navbar />
-      <Landing />
-    </Fragment>
+    <BrowserRouter>
+      <Fragment>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Landing />} />
+        </Routes>
+
+        <div className='container'>
+          <Routes>
+            <Route path='/register' element={<Register />} />
+            <Route path='/login' element={<Login />} />
+          </Routes>
+        </div>
+      </Fragment>
+    </BrowserRouter>
   );
 }
 
